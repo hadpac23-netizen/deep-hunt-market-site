32f7b9f419ce8d3ad83766fc1158f74fc0e09e93
adi chehade <adichehade@adis-MacBook-Air.local>
2026-09-17 16:51:43 +0300

    Add privacy-safe PostHog EU observability

--- analytics-config.js
+++ analytics-config.js
@@ -2,5 +2,9 @@ window.HUNT_ANALYTICS_CONFIG = Object.freeze({
   gtmContainerId: "",
   ga4MeasurementId: "G-894VETFBKE",
   consentRequired: true,
-  environment: "production"
+  environment: "production",
+  posthogProjectToken: "phc_p9SFBWGcTZyMfXdsRnsqnEauK3yhJShhrCmqbf4U7tEs",
+  posthogApiHost: "https://eu.i.posthog.com",
+  posthogUiHost: "https://eu.posthog.com",
+  posthogDefaults: "2026-05-30"
 });
