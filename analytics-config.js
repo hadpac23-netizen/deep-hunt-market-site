62a8f6ecd25d9c4060901f7cb0d3d954f522850a
hadpac23-netizen <hadpac23@gmail.com>
2026-09-20 13:19:27 +0300

    feat(analytics): version privacy consent and add EU observability config

--- analytics-config.js
+++ analytics-config.js
@@ -2,5 +2,12 @@ window.HUNT_ANALYTICS_CONFIG = Object.freeze({
   gtmContainerId: "",
   ga4MeasurementId: "G-894VETFBKE",
   consentRequired: true,
-  environment: "production"
+  consentVersion: "2026-09-20.1",
+  privacyChoicesUrl: "privacy-choices.html",
+  environment: "production",
+  posthogProjectToken: "phc_p9SFBWGcTZyMfXdsRnsqnEauK3yhJShhrCmqbf4U7tEs",
+  posthogApiHost: "https://eu.i.posthog.com",
+  posthogUiHost: "https://eu.posthog.com",
+  posthogDefaults: "2026-05-30",
+  posthogSessionReplay: false
 });
